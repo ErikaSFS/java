c1ebb189a9d2975c98762b4b296823c54a3d5798
Erika Faustino <dossantos.erika08@gmail.com>
2018-07-20 20:27:55 -0300

    Create var.java

---
+++ cod/var.java
@@ -0,0 +1,32 @@
+public class variaveis {
+
+	public static void main(String[] args) {
+                //convenção Java
+                		//convenção Java
+		int idade = 31;
+		String nome = "Erika";
+		String nomeDoMeuGato = "Salém";
+		String ano = "2018";
+		
+		//aceito, mas não utilizado
+		int _idade;
+		int $idade;
+		
+		//não é convenção Java
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		String nomeDoMeucachorro;
+		
+		idade = 31;
+		
+		System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+        System.out.println("nomeDoMeuGato = " + nomeDoMeuGato);
+        System.out.println("ano = " + ano);
+		
+		// má prática
+		int a = 10;
+		String b = "Loiane";
+	}
+
+}
