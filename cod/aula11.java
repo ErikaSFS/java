e4d8676438af031ac7b6839a77fcd93883fa4cfb
Erika Faustino <dossantos.erika08@gmail.com>
2018-07-23 16:27:18 -0300

    Create aula11.java

---
+++ cod/aula11.java
@@ -0,0 +1,20 @@
+
+
+	public static void main(String[] args) {
+		
+		
+		byte idade01 = 20;
+		
+		short idade02 = 34;
+		
+		int idade03 = 21;
+		
+		long idade04 = 32;
+		
+		System.out.println("Valor variável idade01 = " +idade01);
+		System.out.println("Valor variável idade02 = " +idade02);
+		System.out.println("Valor variável idade03 = " +idade03);
+		System.out.println("Valor variável idade04 = " +idade04);
+ 
+	}
+
