d7c9b64d5da9eb9887aca12d790cd337173da0d9
Erika Faustino <dossantos.erika08@gmail.com>
2018-07-23 16:24:36 -0300

    Create aula11b.java

---
+++ cod/aula11b.java
@@ -0,0 +1,13 @@
+public class aula11b {
+
+	public static void main(String[] args) {
+		
+		boolean verdadeiro = true;
+		
+		boolean falso = false;
+		
+		System.out.println("O valoe de verdadeiro é: " + verdadeiro);
+		System.out.println("O valor de falso é: " + falso);
+		
+
+	}
