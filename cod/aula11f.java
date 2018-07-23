1b4a3e0291e24677af1fc8b7b273b3ec65e26677
Erika Faustino <dossantos.erika08@gmail.com>
2018-07-23 16:26:44 -0300

    Create aula11f.java

---
+++ cod/aula11f.java
@@ -0,0 +1,15 @@
+public class aula11f {
+
+	public static void main(String[] args) {
+		
+		double valorPassagem = 3.40;
+		
+		float valorTomate = 1.50f;
+		
+		System.out.println("Valor da Passagem = " +valorPassagem);
+		System.out.println("Valor do Tomate = " +valorTomate);
+		
+		
+	}
+
+}
