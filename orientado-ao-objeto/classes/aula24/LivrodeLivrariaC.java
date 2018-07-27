061b96ef437fcd22720208fc1fcb92bce7872744
Erika Faustino <dossantos.erika08@gmail.com>
2018-07-27 15:23:00 -0300

    Create LivrodeLivrariaC.java

---
+++ orientado-ao-objeto/classes/aula24/LivrodeLivrariaC.java
@@ -0,0 +1,14 @@
+package exercicicoAula24;
+
+public class LivrodeLivraria {
+	 String Titulo;
+	 String Autor;
+	 String Editora;
+	 String Genero;
+	 String Idioma;
+	 long ISBN;
+	 int Edição;
+	 int numPaginas;
+	 
+
+}
